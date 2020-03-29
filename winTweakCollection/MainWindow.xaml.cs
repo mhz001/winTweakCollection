9b61760a2c70f427e0e296bc1c1f63514af0210f
mhz <mhzmastermind@gmail.com>
2020-03-29 14:42:20 +0300

    29.03
    first

---
+++ winTweakCollection/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Diagnostics;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace winTweakCollection
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+
+            string ininame = "LongPathsEnabled" + ".ini";
+
+            if (!Directory.Exists(".\\Tweaks"))
+            {
+                Directory.CreateDirectory(".\\Tweaks");
+
+                INIManager manager = new INIManager(".\\Tweaks\\" + ininame);
+                manager.WritePrivateString("REG", "ROOT", @"HKEY_LOCAL_MACHINE");
+                manager.WritePrivateString("REG", "PATH", @"SYSTEM\CurrentControlSet\Control\FileSystem");
+                manager.WritePrivateString("REG", "NAME", @"HKEY_LOCAL_MACHINE");
+                manager.WritePrivateString("REG", "VALUE", @"0");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _ = MessageBox.Show("nothing");
+        }
+        private void MainWindow_Loaded(object s, RoutedEventArgs e)
+        {
+            string[] inifiles = Directory.GetFiles(".\\Tweaks", "*.ini", SearchOption.TopDirectoryOnly);
+            int count = inifiles.Length;
+            TextBlock[] tbarray = new TextBlock[count];
+            RegTweak[] regTweaks = new RegTweak[count];
+            CheckBox[] checks = new CheckBox[count];
+            
+            for (int i = 0; i < count; i++)
+            {
+                TBox.AppendText(inifiles[i] + "\n");
+                regTweaks[i] = ReadiniFile(inifiles[i]);
+                tbarray[i] = new TextBlock { Text = regTweaks[i].Name + "\t" };
+                checks[i] = new CheckBox { Margin = new Thickness(5 ,5 ,5 ,5) };
+                if (regTweaks[i].ValueExist) 
+                    if (regTweaks[i].NewValue.ToString() != regTweaks[i].KeyValue.ToString())
+                    {
+                        checks[i].IsChecked = true;
+                        tbarray[i].Text += regTweaks[i].keyName;
+                    }
+                    else
+                    {
+                        checks[i].IsEnabled = false;
+                        tbarray[i].Text += "Alredy Done.";
+                    }
+                else
+                {
+                    checks[i].IsChecked = true;
+                    tbarray[i].Text += "not foundet";
+                }
+                stackpan1.Children.Add(tbarray[i]); stackpan1.Children.Add(checks[i]);
+            }
+
+            // LUA ConsentPromptBehaviorAdmin PromptOnSecureDesktop 0 0 0 off  1 5 1 on
+            //[HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel]
+            //"{ 20D04FE0 - 3AEA - 1069 - A2D8 - 08002B30309D}"= dword:00000000
+            //System.IO.Path.GetFileNameWithoutExtension(Process.GetCurrentProcess().MainModule.FileName); //имя без расширения
+            //System.Environment.GetCommandLineArgs()[0];// полный путь
+
+
+        }
+
+        private RegTweak ReadiniFile(string ini)
+        {
+            string ROOT, PATH, NAME, VALUE;
+
+            INIManager manager = new INIManager(ini);
+            ROOT = manager.GetPrivateString("REG", "ROOT");
+            PATH = manager.GetPrivateString("REG", "PATH");
+            NAME = manager.GetPrivateString("REG", "NAME");
+            VALUE = manager.GetPrivateString("REG", "VALUE");
+
+            RegTweak tw = new RegTweak(ROOT, PATH, NAME)
+            {
+                NewValue = VALUE,
+                Name = System.IO.Path.GetFileNameWithoutExtension(ini)
+            };
+
+            //string[] lines = File.ReadAllLines(ini);
+            //foreach (string line in lines)
+            //{
+            //    TBox.AppendText(line + "\n");
+            //}
+
+            return tw;
+        }
+    }
+}
---
+++ winTweakCollection/RegTweak.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace winTweakCollection
+{
+    internal class RegTweak
+    {
+        private string rootKey;
+        private string subKey;
+        public string keyName;
+        private string valueName;
+        private readonly RegistryView rv = RegistryView.Default;
+        private RegistryHive rh = RegistryHive.LocalMachine;
+        private bool valueExist;
+        private object keyValue;
+        private RegistryValueKind keyType;
+        private object newValue = null;
+
+        public RegTweak(string subKey, string valueName): this ("HKLM", subKey, valueName)  {  }
+
+        public RegTweak(string rootKey, string subKey, string valueName)
+        {
+            Name = "key";
+            this.rootKey = rootKey;
+            this.subKey = subKey;
+            this.valueName = valueName;
+            keyName = $"{rootKey}\\{subKey}";
+            //if ((rootKey == "HKLM") || (rootKey == "HKEY_LOCAL_MACHINE"))
+            //    rh = RegistryHive.LocalMachine;
+            if ((rootKey == "HKCU") || (rootKey == "HKEY_CURRENT_USER"))
+                rh = RegistryHive.CurrentUser;
+            if ((rootKey == "HKCU") || (rootKey == "HKEY_CLASSES_ROOT"))
+                rh = RegistryHive.ClassesRoot;
+            RegistryKey key = RegistryKey.OpenBaseKey(rh, rv).OpenSubKey(subKey);
+            keyValue = key.GetValue(valueName);
+            if (keyValue is null)
+            {
+                valueExist = false;
+                keyType = RegistryValueKind.DWord;
+                keyValue = 0;
+            }
+            else
+            {
+                valueExist = true;
+                keyType = key.GetValueKind(valueName);
+            }
+
+        }
+
+        public string Name { get; set; }
+        public string RootKey { get => rootKey; set => rootKey = value; }
+        public string SubKey { get => subKey; set => subKey = value; }
+        public string ValueName { get => valueName; set => valueName = value; }
+        public bool ValueExist { get => valueExist; set => valueExist = value; }
+        public RegistryValueKind KeyType { get => keyType; set => keyType = value; }
+        public object KeyValue { get => keyValue; }
+        public object NewValue { get => newValue; set => newValue = value; }
+
+        public void SaveNewValue()
+        {
+            try
+            {
+                RegistryKey key = RegistryKey.OpenBaseKey(rh, rv).OpenSubKey(subKey, true);
+                key.SetValue(valueName, newValue, keyType);
+            }
+            catch (ArgumentNullException e)
+            {
+                _ = MessageBox.Show(e.Message); //Свойство value имеет значение null.
+            }
+            catch (ArgumentException e)
+            {
+                _ = MessageBox.Show(e.Message); //Значение параметра value является неподдерживаемым типом данных.
+            }
+            catch (ObjectDisposedException e)
+            {
+                _ = MessageBox.Show(e.Message); //Объект RegistryKey, содержащий заданное значение, закрыт(доступ к закрытым разделам отсутствует).
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                _ = MessageBox.Show(e.Message); //Раздел RegistryKey является разделом только для чтения, и запись в него невозможна. Например, этот раздел не был открыт с доступом для записи.
+            }
+        }
+    }
+}
